8502abc9be9e2a4a0f6238ee01ece407a88542b9
yangdanny97 <dzy4@cornell.edu>
2021-07-25 02:09:00 -0500

    update main

--- main.py
+++ main.py
@@ -2,37 +2,30 @@ import argparse
 import json
 from test import run_all_tests, run_parse_tests, run_typecheck_tests
 from compiler.compiler import Compiler
-from compiler.parser import Parser
-from compiler.typechecker import TypeChecker
+from compiler.builder import Builder
 from compiler.astnodes import Node
 
+mode_help = (
+    'modes:\n' +
+    'parse - output AST before typechecking\n' +
+    'tc - output typechecked AST\n' +
+    'python - output untyped Python 3 source code'
+)
 
 def main():
     parser = argparse.ArgumentParser(description='Chocopy frontend')
-    parser.add_argument('-m', '--mode', dest='mode', choices=["parse", "tc", "llvm"], default="tc",
-                        help='modes:\nparse (output AST before typechecking)\ntc (output typechecked AST)\nllvm (output LLVM IR)')
-    parser.add_argument('-o', '--output', dest='output', action='store_false',
+    parser.add_argument('-m', '--mode', dest='mode', choices=["parse", "tc", "python"], default="python",
+                        help=mode_help)
+    parser.add_argument('-p', '--print', dest='print', action='store_false',
                         help="output to stdout instead of file")
-    parser.add_argument('--test-all', dest='testall', action='store_true',
+    parser.add_argument('--test', dest='test', action='store_true',
                         help="run all test cases")
-    parser.add_argument('--test-parse', dest='testparse', action='store_true',
-                        help="run parser test cases")
-    parser.add_argument('--test-tc', dest='testtc', action='store_true',
-                        help="run typechecker test cases")
     parser.add_argument('infile', nargs='?', type=str, default=None)
     parser.add_argument('outfile', nargs='?', type=str, default=None)
     args = parser.parse_args()
 
-    if args.testall:
+    if args.test:
         run_all_tests()
-        return
-
-    if args.testparse:
-        run_parse_tests()
-        return
-
-    if args.testtc:
-        run_typecheck_tests()
         return
 
     infile = args.infile
@@ -47,6 +40,8 @@ def main():
             outfile = infile + ".ast.typed"
         elif args.mode == "parse":
             outfile = infile + ".ast"
+        elif args.mode == "python":
+            outfile = infile + ".out.py"
 
     compiler = Compiler()
     astparser = compiler.parser
@@ -66,15 +61,17 @@ def main():
 
     if args.mode in {"parse", "tc"}:
         ast_json = tree.toJSON(False)
-        if args.output:  # output to file
+        if args.print:  # output to file
             with open(outfile, "w") as f:
                 json.dump(ast_json, f, indent=2)
         else:  # output to stdout
             if isinstance(tree, Node):
                 print(json.dumps(ast_json, indent=2))
-    elif args.mode == "llvm":
-        pass  # TODO
-
+    elif args.mode == "python":
+        builder = Builder()
+        tree.getPythonStr(builder)
+        with open(outfile, "w") as f:
+            f.write(builder.emit())
 
 if __name__ == "__main__":
     main()
